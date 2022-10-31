b172f16ae66b736e862e7205e54ce426954abf5e
fatmatanis <fatmatanis10@gmail.com>
2022-10-31 13:00:51 +0300

    getStaticProps and image added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
 
 import classes from "../../styles/Header.module.css";
 
 const Header = () => {
-  const router = useRouter();
-
   return (
     <div className={classes.header}>
       <nav className={classes.container}>
--- pages/comment/[id].tsx
+++ pages/comment/[id].tsx
@@ -34,3 +34,59 @@ const SingleComment = () => {
 };
 
 export default SingleComment;
+
+//
+//
+// import { GetStaticProps, InferGetStaticPropsType } from "next";
+// import { useRouter } from "next/router";
+// import { ParsedUrlQuery } from "querystring";
+// import React, { useEffect, useState } from "react";
+// import { IComment } from "..";
+
+// const SingleComment = ({
+//   commentData,
+// }: InferGetStaticPropsType<typeof getStaticProps>) => {
+
+//   return (
+//     <div style={{ padding: "48px" }}>
+//       <h2 style={{ color: "red" }}>Single Comment</h2>
+//       <p>
+//         <b>Full Comment: </b> {commentData.name}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default SingleComment;
+
+// export interface IParams extends ParsedUrlQuery {
+//   id: string;
+// }
+
+// export const getStaticProps: GetStaticProps<{ commentData: IComment }> = async (
+//   context
+// ) => {
+//   const params = context.params as IParams;
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/comments/${params.id}`
+//   );
+//   const commentData = await res.json();
+
+//   return {
+//     props: {
+//       commentData,
+//     },
+//   };
+// };
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/comments/`);
+//   const data = await res.json();
+//   const ids = data.map((comment: IComment) => comment.id);
+//   const paths = ids.map((id: number) => ({ params: { id: id.toString() } }));
+
+//   return {
+//     paths,
+//     fallback: false, // can also be true or 'blocking'
+//   };
+// }
